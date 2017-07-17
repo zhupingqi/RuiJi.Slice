3c5cdc37ef5582ccf69c4989737786380da9c25d
zhupingqi <416803633@qq.com>
2017-07-17 20:29:10 +0800

    添加开源LGPL开源协议

--- RuiJi.Slicer.Core/ImageMould/IImageMould.cs
+++ RuiJi.Slicer.Core/ImageMould/IImageMould.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿/*
+This file is part of RuiJi.Slice: A library for slicing 3D model.
+RuiJi.Slice is part of RuiJiHG: RuiJiHG is holographic projection.
+see http://www.ruijihg.com/ for more infomation.
+
+Copyright (C) 2017 Pingqi(416803633@qq.com)
+Copyright (c) 2017, githublixiang(271800249@qq.com)
+
+RuiJi.Slice is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+RuiJi.Slice is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with wiringPi.
+If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
--- RuiJi.Slicer.Core/SlicePanelInfo.cs
+++ RuiJi.Slicer.Core/SlicePanelInfo.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿/*
+This file is part of RuiJi.Slice: A library for slicing 3D model.
+RuiJi.Slice is part of RuiJiHG: RuiJiHG is holographic projection.
+see http://www.ruijihg.com/ for more infomation.
+
+Copyright (C) 2017 Pingqi(416803633@qq.com)
+Copyright (c) 2017, githublixiang(271800249@qq.com)
+
+RuiJi.Slice is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+RuiJi.Slice is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with wiringPi.
+If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
@@ -9,12 +32,18 @@ namespace RuiJi.Slicer.Core
 {
     public class SlicePanelInfo
     {
+        /// <summary>
+        /// 初始面
+        /// </summary>
         public Plane Plane
         {
             get;
             private set;
         }
 
+        /// <summary>
+        /// 旋转角度
+        /// </summary>
         public float Angle
         {
             get;
@@ -22,9 +51,27 @@ namespace RuiJi.Slicer.Core
         }
 
         /// <summary>
-        /// 原始旋转轴
+        /// 旋转轴
         /// </summary>
         public Vector3 Axis
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 初始面X坐标轴
+        /// </summary>
+        public Vector3 PlaneAxisX
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 初始面Y坐标轴
+        /// </summary>
+        public Vector3 PlaneAxisY
         {
             get;
             private set;
