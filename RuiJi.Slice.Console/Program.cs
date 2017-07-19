9f456d03b4b37936a09eaccc6cf02c3d10412bc3
lixianggithub <271800249@qq.com>
2017-07-20 00:15:58 +0800

    对角线及控制台切片工具

--- RuiJi.Slice.Console/Program.cs
+++ RuiJi.Slice.Console/Program.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Drawing.Imaging;
 using System.Linq;
 using System.Numerics;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,11 +21,45 @@ namespace ConsoleApplication1
             Console.ReadLine();
         }
 
+        /// <summary>
+        /// 控制台切片工具(未完成)
+        /// </summary>
+        static void ConsoleSlicer()
+        {
+            Console.WriteLine("----------------------切片工具----------------------");
+            Console.WriteLine("请输入要切片的3D图绝对路径：");
+            var path = Console.ReadLine();
+            while (true)
+            {
+                if (!Path.IsPathRooted(path))
+                {
+                    Console.WriteLine("请输入正确的地址：");
+                    path = Console.ReadLine();
+                }
+                else
+                    break;
+            }
+            //Console.WriteLine("请输入法线的X分量");
+            //var x = Console.ReadLine();
+            //Console.WriteLine("请输入法线的Y分量");
+            //var y = Console.ReadLine();
+            //Console.WriteLine("请输入法线的Z分量");
+            //var z = Console.ReadLine();
+            //Console.WriteLine("请输入平面从原点沿其法线的距离");
+            //var d = Console.ReadLine();
+            Console.WriteLine("请输入切片数量");
+            var count = Console.ReadLine();
+            Console.WriteLine("请输入切片弧度");
+            var angle = Console.ReadLine();
 
+        }
 
+        /// <summary>
+        /// 测试绕轴圆周切片
+        /// </summary>
         static void TestArrayCreate()
         {
-            var p = new Plane(0,1,0,1);
+            var p = new Plane(0, 1, 0, 1);
             var d = new ArrayDefine(p, ArrayType.Circle, 8);
             var c = new CircleArrayCreater();
             var ps = c.CreateArrayPlane(d);
@@ -37,6 +72,9 @@ namespace ConsoleApplication1
             Console.WriteLine("TestArrayCreate finish");
         }
 
+        /// <summary>
+        /// 测试读取图片并切片
+        /// </summary>
         static void TestSlicer()
         {
             //-0.03141076,0.9995066
@@ -77,6 +115,9 @@ namespace ConsoleApplication1
             Console.WriteLine("TestSlicer finish");
         }
 
+        /// <summary>
+        /// 测试转为2D图形
+        /// </summary>
         static void TestTo2D()
         {
             //var p = SliceImage.To2D(new Plane(0,1,0,8),new Vector3(-1,8,1));
